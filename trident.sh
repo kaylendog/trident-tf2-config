132be327d5b8681e04958ce8425333d811068dfc
skyefoxie <actuallyori@gmail.com>
2020-05-30 20:02:22 +0100

    root-based installation

--- trident.sh
+++ trident.sh
@@ -5,17 +5,35 @@ SUBJECT=3621
 echo Trident Configuration
 echo version $VERSION updated $UPDATED; echo
 
-echo info: Checking dependancies...
-require_dep wget
+echo info: Downloading OS dependancies...
+dpkg --add-architecture i386; 
+apt update; 
+apt install -y unzip binutils jq netcat lib32gcc1 lib32stdc++6 steamcmd libcurl4-gnutls-dev:i386 libtcmalloc-minimal4:i386
+
+echo info: Creating user 'tf2'...
+useradd tf2
+su tf2
+
+# Enter into TF2 home directory.
+cd /home/tf2
 
 if [ ! -d "./lgsm" ] && [ ! -f "tf2server" ]; then
-    echo "error: LGSM does not appear to be installed - downloading it for you..."
-    wget -qO linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh
-    echo "info: Run ./linuxgsm.sh tf2server to continue."
-    exit
+    echo "error: LGSM does not appear to be installed - downloading it for you."
+    curl -o- https://linuxgsm.sh | bash -s tf2server
 fi
 
-GAME_DIRECTORY=$1
+# Install
+echo "info: Starting server installation..."
+
+if [ ! $1 ]; then
+    echo warn: Game server token has not been specified - this can be changed later.
+fi
+
+printf "%s\n" Y Y $GSLT_TOKEN Y Y | ./tf2server install
+
+exit;
+
+GAME_DIRECTORY=/home/tf2/serverfiles
 
 # Check for missing game directory.
 if [ ! -d $GAME_DIRECTORY ]; then
